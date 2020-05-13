4a959839bc78919f301f4594febd3148dd03ec6a
Greg Answer <greganswer@gmail.com>
2020-05-13 17:53:09 -0400

    Add command description

--- cmd/root.go
+++ cmd/root.go
@@ -2,8 +2,9 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
 	"os"
+
+	"github.com/spf13/cobra"
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
@@ -13,17 +14,18 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "laptop",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Use:     "laptop",
+	Version: version,
+	Short:   "Laptop setup and update script for Mac users",
+	Long: `Setup the following on your new Mac:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+    - HomeBrew
+    - Dot files
+    - Native apps (Chrome, Spotify, Vs Code, etc.)
+    - CLI apps (Git, Hub, Docker-compose, etc.)
+    - Local Databases (Postgres, Redis, etc.)
+    - Settings for your apps
+`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
